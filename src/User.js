2350e35689bd2bf01851c644809c7edd3c22c3b6
seangaug <seanjnr1991@aol.com>
2023-02-10 17:19:20 +0000

    user calss - initialisation & login method test added

--- src/User.js
+++ src/User.js
@@ -1,12 +1,28 @@
 class User {
-  constructor(username,password,age,loggedIn){
+  constructor(username, password, age, loggedIn) {
+
+    // username, password, and age are provided to the constructor as arguments. loggedIn represents whether the user is currently logged in.
+    // A user is NOT logged in when they first register.
+
     this.username = username
     this.password = password
     this.age = age
     this.loggedIn = loggedIn
   }
-  login(password){}
-  logout() {}
+  
+  login(password){
+    // If password is correct, logs the User in. If not, throws incorrect password error.
+    if(password === this.password) {
+      this.loggedIn = true;
+    } else {
+      throw new Error("incorrect password")
+    }
+  }
+  
+  logout() {
+    // Logs the User out.
+
+  }
 }
 
 module.exports = User
--- tests/User.test.js
+++ tests/User.test.js
@@ -1,13 +1,30 @@
 const User = require('../src/User')
 
-// User tests here
+describe("user instance integrity checks", () => {
+    const user1 = new User("RachaelB", "password1", 27, false);
 
-// test username
+    // User class username, password, age, and loggedIn properties initialized correctly
+    test("User class username, password, age, and loggedIn properties initialized correctly", () => {
+        expect(user1).toHaveProperty("username");
+        expect(typeof user1.username).toBe("string");
+        expect(typeof user1.password).toBe("string");
+        expect(typeof user1.age).toBe("number");
+        expect(typeof user1.loggedIn).toBe("boolean");
+    })
 
-// test password
-
-// test age
-
-// test login
+    // User class has login and logout methods
+    test("User class has login and logout methods", () => {
+        expect(user1.login).toBeDefined();
+        expect(typeof user1.login).toBe('function');
+        expect(user1.logout).toBeDefined();
+        expect(typeof user1.logout).toBe('function');
+    })
+    
+    // User cannot login if incorrect password provided
+    const user2 = new User("userSteve", "passwordSteve", 25, false)
+    test("User cannot login if incorrect password provided", () => {
+        expect(() => user2.login("wrongPassword")).toThrow("incorrect password");
+    })
+})
 
 // test logout
