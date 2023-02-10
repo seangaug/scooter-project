25d47d207a7e1aaabba6bf9cb2ad2ed38fd4c260
seangaug <seanjnr1991@aol.com>
2023-02-10 12:00:49 +0000

    scooter class nextSerial and method test for rent / dock added

--- tests/Scooter.test.js
+++ tests/Scooter.test.js
@@ -2,13 +2,14 @@ const Scooter = require('../src/Scooter')
 const User = require('../src/User')
 
 describe('scooter instance integrity checks', () => {
-  let scooter1 = new Scooter("Manchester")
+  const scooter1 = new Scooter("Manchester")
   
   //typeof scooter === object
   test('New instance of scooter created as an object', () => {
       expect(typeof scooter1).toBe("object");
     });
   
+  // Scooter class station, user, serial, charge, and isBroken properties initialized correctly
   test("Instance has correct properties", () => {
     expect(scooter1).toHaveProperty("user", null);
     expect(scooter1).toHaveProperty("charge");
@@ -16,18 +17,28 @@ describe('scooter instance integrity checks', () => {
     expect(typeof scooter1.isBroken).toBe("boolean");
     expect(scooter1.station).toBe("Manchester")
   });
+
+// Scooter class has nextSerial class property used to assign unique serial numbers
+  test("nextSerial class property used to assign unique serial numbers", () => {
+    const scooter2 = new Scooter("Trafford")
+    expect(typeof scooter2.nextSerial++).toBe("number");
+    // expect(scooter2.serial).toBe(scooter1.serial + 1);
+  })
 })
 
-//Method tests
+// Method tests
 describe('scooter methods', () => {
-  // tests here!
+  const scooter3 = new Scooter()
 
-  //rent method
-
-  //dock method
-
+// Scooter class has rent and dock methods
+  test("scooter has class and rent methods", () => {
+    expect(scooter3.rent).toBeDefined();
+    expect(typeof scooter3.rent).toBe('function');
+    expect(scooter3.dock).toBeDefined();
+    expect(typeof scooter3.dock).toBe('function');
+   })
+  
   //requestRepair method
 
   //charge method
-
 })
